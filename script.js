988398bea1312fc423e1035e6ffc4984db2d8607
Jhagan B <63654882+jhaganb@users.noreply.github.com>
2025-08-06 19:49:55 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
 console.log("loaded");
 // document.querySelector("h1").style.color = "red";
-document.getElementById("test1").style.color = "red";
+document.getElementById("test1").style.color = "blue";
+
